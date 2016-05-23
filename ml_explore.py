5dfe1cac94e485d7eab1cd92b585fba3f6fc3c13
ajithpad <ajith.physics@gmail.com>
2016-05-23 16:38:14 +0530

    New ml_explore.py

--- ml_explore.py
+++ ml_explore.py
@@ -24,22 +24,41 @@ excel_val = pd.read_excel(data_path + 'condom_study.xlsx')
 excel_val_15 = excel_val[np.logical_or(excel_val['Q15']==4,excel_val['Q15']==8)]
 
 #Remove ids for testing
-test_idx = random.sample(range(76),3)
+#test_idx = random.sample(range(76),3)
+#
+#train_val = excel_val_15.drop(excel_val_15.index[test_idx])
+#train_target = train_val['Q15']
+#train_data = train_val.ix[:,:4]
+#
+#clf = tree.DecisionTreeClassifier()
+#clf = clf.fit(train_data,train_target)
+#
+#test_val = excel_val_15.iloc[test_idx]
+#test_target = test_val['Q15']
+#test_data = test_val.ix[:,:4]
+#
+#scores = cross_validation.cross_val_score(clf, train_data, train_target, cv = 10)
+#
+#print clf.predict(test_data)
+#print test_target
+#print scores.mean()
+#print scores.std()
+X = excel_val_15.ix[:,:4]
+y = excel_val_15['Q15']
 
-train_val = excel_val_15.drop(excel_val_15.index[test_idx])
-train_target = train_val['Q15']
-train_data = train_val.ix[:,:4]
+
+from sklearn.cross_validation import train_test_split
+X_train,X_test, y_train, y_test = train_test_split(X,y,test_size = 0.4)
 
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(train_data,train_target)
+clf = clf.fit(X_train,y_train)
 
-test_val = excel_val_15.iloc[test_idx]
-test_target = test_val['Q15']
-test_data = test_val.ix[:,:4]
+predictions = clf.predict(X_test)
 
-scores = cross_validation.cross_val_score(clf, train_data, train_target, cv = 10)
+from sklearn.metrics import accuracy_score
+print accuracy_score(y_test, predictions)
 
-print clf.predict(test_data)
-print test_target
+scores = cross_validation.cross_val_score(clf, X_train, y_train, cv = 10)
+
 print scores.mean()
 print scores.std()
